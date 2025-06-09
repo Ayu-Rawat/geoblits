fde52c6843be9d55b189e6306113890a5f9201d4
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-06-09 18:45:27 +0530

    fixed a minor isssue

--- app/api/user/store-user/route.js
+++ app/api/user/store-user/route.js
@@ -54,7 +54,7 @@ export async function POST(req) {
     }
   }
 
-  let email = user.email
+  let email = user.name || user.email;
 
   if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
     email = email + '@gmail.com'; 
